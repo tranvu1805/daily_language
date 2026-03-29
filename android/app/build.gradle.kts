048551b320c44f3467a0845d96530a1e0dd30f4e
tranvu <trandinhvu1805@gmail.com>
2026-03-29 10:59:40 +0700

    feat(notification): add notification, alarm, and boot permissions with receiver integration and desugaring support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -41,7 +42,11 @@ android {
         }
     }
 }
-
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    implementation("androidx.window:window:1.0.0")
+    implementation("androidx.window:window-java:1.0.0")
+}
 flutter {
     source = "../.."
 }
